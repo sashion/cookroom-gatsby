7c9dee413a7641650cad8ef5e98327f15d16ccce
Sacha Hertel <mail@sashion.de>
2022-05-19 14:04:53 +0200

    hello cooky 2

--- src/pages/index.js
+++ src/pages/index.js
@@ -83,7 +83,7 @@ const IndexPage = () => (
         style={{ marginBottom: `var(--space-3)` }}
       />
       <h1>
-        Welcome to <b>Cookroom! :)</b>
+        Welcome to <b>Cookroom! xxx :)</b>
       </h1>
       <p className={styles.intro}>
         <b>Example pages:</b>{" "}
