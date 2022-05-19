86661fd31f24d7ca4c62aa122864400e9d75bd7a
Sacha Hertel <mail@sashion.de>
2022-05-19 14:17:43 +0200

    add cookroom links

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,28 +8,22 @@ import * as styles from "../components/index.module.css"
 
 const links = [
   {
-    text: "Tutorial",
-    url: "https://www.gatsbyjs.com/docs/tutorial",
+    text: "Cookroom light – Kochevents ohne Koch",
+    url: "http://e1.h0t3l.de",
     description:
       "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
   },
   {
-    text: "Examples",
-    url: "https://github.com/gatsbyjs/gatsby/tree/master/examples",
+    text: "Cookroom - Kochevents mit Koch",
+    url: "http://e2.h0t3l.de",
     description:
       "A collection of websites ranging from very basic to complex/complete that illustrate how to accomplish specific tasks within your Gatsby sites.",
   },
   {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
+    text: "Grillevents",
+    url: "http://e3.h0t3l.de",
     description:
       "Learn how to add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-  },
-  {
-    text: "Build and Host",
-    url: "https://www.gatsbyjs.com/cloud",
-    description:
-      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
   },
 ]
 
